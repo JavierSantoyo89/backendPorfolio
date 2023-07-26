d1c6cf5465b93688ce50da9ad99d8457b00c696c
Javier Santoyo <javier.santoyo@yahoo.com>
2023-07-26 16:19:37 -0600

    Bug: Edit API

--- src/controllers/mainController.ts
+++ src/controllers/mainController.ts
@@ -1,15 +1,17 @@
-import {Request, Response, NextFunction} from "express"
-const toThousand = (n: { toString: () => string; }) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+import { Request, Response, NextFunction } from "express"
+const toThousand = (n: { toString: () => string }) =>
+  n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
 
 const mainController = {
-    index: (req: Request,res: Response) =>{
-        // res.send('jala')
-        res.render('index')
-    },
-    notfound: (_req: Request,res:Response) =>{
-        res.render('not-found')
-        // res.send('No existe este sitio, animal!!!')
-    }
+  index: (req: Request, res: Response) => {
+    // res.send('jala')
+    // res.render('index')
+    res.status(200).json({ Total: 100 })
+  },
+  notfound: (_req: Request, res: Response) => {
+    res.render("not-found")
+    // res.send('No existe este sitio, animal!!!')
+  },
 }
 
 export default mainController
