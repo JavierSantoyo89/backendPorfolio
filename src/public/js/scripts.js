d7c1a88f57de1fd1bbdca6e73c2373f525147fea
Javier Santoyo <javier.santoyo@yahoo.com>
2023-09-11 00:14:46 -0600

    Install toastjs

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,1 +1,16 @@
-console.log("Prueba de javascript en view");
+function copyClipBoard(IdCopy){
+  var copyText = document.getElementById(`${IdCopy}`); 
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+  navigator.clipboard.writeText(copyText.value);
+  
+  Toastify({
+    text: `Ruta ${IdCopy} copiada`,
+    className: "info",
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    }
+  }).showToast();
+
+}
+
