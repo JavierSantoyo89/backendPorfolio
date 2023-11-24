ded876efc0c3287d5e352be6e2cf1a9656cb614b
Javier santoyo <javier.santoyo@yahoo.com>
2023-11-24 00:24:50 -0600

    📦 NEW: Create stadistics endpoint

--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -7,5 +7,6 @@ router.get('/', mainController.index)
 router.get('/privacidad', mainController.Privacidad)
 router.get('/agradecimiento', mainController.Agradecimiento)
 router.get('/search', mainController.search)
+router.get('/stadistics',mainController.stadistics)
 
 export default router
