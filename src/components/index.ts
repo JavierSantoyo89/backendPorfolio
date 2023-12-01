0383e9ce43bbd04e35a4517c818105b1207e91de
Javier santoyo <javier.santoyo@yahoo.com>
2023-12-01 00:27:07 -0600

    📦 NEW: Insert favicon

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+export * from './curameController';
+export { default as mainController } from './mainController';
+export * from './metalMecanicaController';
+export * from './old/metalMecanicaController copy 2';
+export * from './old/metalMecanicaController copy';
+export { default as pruebas } from './old/pruebas';
+export * from './old/queryMetalMecanica';
+export * from './query';
+export * from './restaurantController';
--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,17 +1,17 @@
-function copyClipBoard(IdCopy){
-  var copyText = document.getElementById(`${IdCopy}`); 
-  copyText.select(`${IdCopy}`);
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-  navigator.clipboard.writeText(copyText.value);
+
+//* -------------------- 
+let host = "https://backendportfolio-r0uf.onrender.com/"
+function copyClipBoard(apiTitle, proyect, endpointName){
+  console.log(host+proyect+endpointName);
+  navigator.clipboard.writeText(host+proyect+endpointName);
   
   Toastify({
-    text: `Ruta ${IdCopy} copiada`,
+    text: `Ruta ${apiTitle} copiada`,
     className: "info",
     style: {
       background: "linear-gradient(to right, #00b09b, #96c93d)",
     }
   }).showToast();
-
 }
 
 function Scroll_Top(){
