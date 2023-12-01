be69eaec078ad0a745e81339eb66ed79cb863ae5
Javier santoyo <javier.santoyo@yahoo.com>
2023-12-01 00:42:37 -0600

    🐛 FIX: Update index.ts from components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,5 @@
 export * from './curameController';
+export * from './index';
 export { default as mainController } from './mainController';
 export * from './metalMecanicaController';
-export * from './old/metalMecanicaController copy 2';
-export * from './old/metalMecanicaController copy';
-export { default as pruebas } from './old/pruebas';
-export * from './old/queryMetalMecanica';
-export * from './query';
 export * from './restaurantController';
