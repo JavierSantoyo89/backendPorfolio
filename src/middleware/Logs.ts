e0fd98885627aa71cc51d508a7c71b6e7b6d387a
Javier Santoyo <javier.santoyo@yahoo.com>
2024-03-10 01:06:24 -0600

    fix bux on statistics controller

--- src/middleware/Logs.ts
+++ src/middleware/Logs.ts
@@ -4,44 +4,58 @@ var dateActual = new Date();
 // var dataQuerys: any = null;
 // var ErrorQuery: any = null
 
+export interface ILogs {
+  TableName: string,
+  CRUDtype: string,
+  dataQuerys: any,
+  ErrorQuery: any,
+  data: Datum[]
+  }
+
+interface Datum {
+  status: string
+  crud: string
+  table: string
+  project: string
+}
+
 export function Logs(
   TableName: string,
   CRUDtype: string,
   dataQuerys: any,
-  ErrorQuery: any
+  ErrorQuery: any,
 ) {
   console.log(TableName);
 
   async function main(TableName: string, dataQuerys: any) {
     dataQuerys;
     console.log("entro al main");
-
     console.log(TableName);
   }
   main(TableName, dataQuerys)
     .then(async () => {
-      const ok = await prisma.tblstadistics.create({
-        data: {
-          status: "Ok",
-          crud: CRUDtype,
-          table: TableName,
-          project: "Metal-Mecanica",
+      const ok  = await prisma.tbl_statistics.create({
+        data : {
+          status: "Ok" as any,
+          crud: CRUDtype as any,
+          table: TableName as any,
+          project: "Metal-Mecanica" as any,
           datetime: dateActual,
-        },
+        } as any,
       });
       console.log(ok);
       await prisma.$disconnect();
     })
     .catch(async () => {
       ErrorQuery;
-      const fail = await prisma.tblstadistics.create({
+      const fail = await prisma.tbl_statistics.create({
         data: {
-          status: "Fail",
-          crud: CRUDtype,
-          table: TableName,
-          project: "Metal-Mecanica",
+          status: "Fail" as any,
+          crud: CRUDtype  as any,
+          table: TableName as any,
+          project: "Metal-Mecanica" as any,
           datetime: dateActual,
-        },
+        } as any,
       });
       console.log(fail);
       await prisma.$disconnect();
